ac0a8f744691bb7be8123def7fccbc73d053e600
alionachernenko <errorroror75@gmail.com>
2024-06-01 16:12:52 +0300

    version 1.0

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	fileNames := getFileNames()
+	notes := getNotes(fileNames)
+
+	var searchInput string
+
+	fmt.Println("What do you want to find?")
+
+	fmt.Scan(&searchInput)
+
+	searchNotes(notes, searchInput)
+}
+
+func getFileNames() []string {
+	files, err := os.ReadDir("./")
+
+	if err != nil {
+		fmt.Printf("Error reading directory :(")
+		return nil
+	}
+
+	var fileNames []string
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		}
+
+		fileName := file.Name()
+
+		if strings.HasSuffix(fileName, ".txt") {
+			fileNames = append(fileNames, fileName)
+		}
+	}
+
+	return fileNames
+}
+
+func getNotes(fileNames []string) []string {
+	var notes []string
+
+	for _, noteName := range fileNames {
+		note, err := os.Open(noteName)
+
+		if err != nil {
+			fmt.Printf("Error reading note %v", note)
+
+			continue
+		}
+
+		defer note.Close()
+
+		scanner := bufio.NewScanner(note)
+
+		for scanner.Scan() {
+			line := scanner.Text()
+			notes = append(notes, line)
+		}
+	}
+
+	return notes
+}
+
+func searchNotes(notes []string, input string) {
+	for _, note := range notes {
+		if strings.Contains(strings.ToLower(note), input) {
+			fmt.Printf("%v\n\n", note)
+		}
+	}
+}
